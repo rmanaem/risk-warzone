7565eb3232d17d638a7bfa4ca2e7dc6bea4cac08
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-10-01 17:13:11 -0400

    [#1] Implement constructors declared in Territory.h file

--- Map/Territory.cpp
+++ Map/Territory.cpp
@@ -1,5 +1,18 @@
 #include "./Territory.h"
 
+//-------------- Constructors --------------//
+    Territory::Territory(){}
+
+    Territory::Territory(string territoryName, int ownerId, int numberOfArmies){
+        this->territoryName = territoryName;
+        this->ownerId = ownerId;
+        this->numberOfArmies = numberOfArmies;
+    }
+    
+    Territory::Territory(string territoryName){
+        this->territoryName = territoryName;
+    }
+
 //-------------- Getters --------------//
     string Territory::getTerritoryName(){
         return territoryName;
@@ -17,11 +30,10 @@
     void Territory::setTerritoryName(string newTerritoryName){
         territoryName = newTerritoryName;
     }
-    
     void Territory::setOwnerId(int newOwnerId){
         ownerId = newOwnerId;
     }
-
+    
     void Territory::setNumberOfArmies(int newNumberOfArmies){
         numberOfArmies = newNumberOfArmies;
     }
