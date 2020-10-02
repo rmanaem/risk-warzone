e4dc72b52c29b70c3de7741c91f19c841b8e2c49
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-10-02 17:29:32 -0400

    [#1] Validate Territory class functions by writing a main method that uses all the functions

--- Map/Territory.cpp
+++ Map/Territory.cpp
@@ -37,3 +37,23 @@ void Territory::setOwnerId(int newOwnerId){
 void Territory::setNumberOfArmies(int newNumberOfArmies){
     numberOfArmies = newNumberOfArmies;
 }
+
+// int main(){
+//     Territory ter1;
+//     Territory ter2("B",1,7);
+//     Territory ter3("C");
+
+//     cout<<ter1.getTerritoryName()<<endl;
+//     cout<<ter3.getTerritoryName()<<endl;
+//     cout<<ter2.getOwnerId()<<endl;
+//     cout<<ter2.getNumberOfArmies()<<endl;
+
+//     ter1.setTerritoryName("Yanbu");
+//     cout<<"\n"+ter1.getTerritoryName()<<endl;
+//     ter1.setOwnerId(19);
+//     cout<<ter1.getOwnerId()<<endl;
+//     ter1.setNumberOfArmies(1);
+//     cout<<ter1.getNumberOfArmies()<<endl;
+
+//     return 0;
+// }
