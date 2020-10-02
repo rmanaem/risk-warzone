5bb4e6bbc7c52a43fd2cea943c08f3d88d6d9c3b
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-10-01 20:37:11 -0400

    Add all declarations for the Graph and Node classes

--- Map/Map.h
+++ Map/Map.h
@@ -3,33 +3,66 @@ using namespace std;
 #include "./Territory.h"
 #include <vector>
 
-class Node{
-    private:
-        Territory data; //A territory object
-        vector<string> E; //A vector of edges
-    public:
-        //-------------- Constructors --------------//
-        Node();
-        Node(Territory data, string edge);
+// class Node{
+//     private:
+//         Territory data; //A territory object
+//         vector<string> E; //A vector of edges
+//     public:
+//         //-------------- Constructors --------------//
+//         Node();
+//         Node(Territory data);//Create a node without connecting it
+//         Node(Territory dataA, Territory dataB);//Creates and connects two nodes
 
-        //-------------- Getters --------------//
-        Territory getData();
-        vector<string> getE(); //get edges
+//         //-------------- Getters --------------//
+//         Territory getData();
+//         vector<string> getE(); //Get edges
 
-        //-------------- Setters --------------//
-        void setData(Territory data);
-};
+//         //-------------- Setters --------------//
+//         void setData(Territory data);
+
+//         //-------------- Other functions --------------//
+//         static void connectTwoNodes(Node A, Node B); //Invokes addEdge function for A and B nodes
+    
+//     private:
+//         void addEdge(string edge);//Add edge to E vector
+// };
 
 class Graph{
+    
+    class Node{
+        Territory data; //A territory object
+        vector<string> E; //A vector of edges
+        public:
+            //-------------- Constructors --------------//
+            Node();
+            Node(Territory data);//Create a node without connecting it
+            Node(Territory dataA, Territory dataB);//Creates and connects two nodes
+
+            //-------------- Getters --------------//
+            Territory getData();
+            vector<string> getE(); //Get edges
+
+            //-------------- Setters --------------//
+            void setData(Territory data);
+
+            //-------------- Other functions --------------//
+            void addEdge(string edge);//Add edge to E vector
+    };
+
     private:
         vector<Node> V; //A vector of vertices (i.e. Territories)
 
     public:
         //-------------- Constructors --------------//
-        Graph(int numberOfNodes);
-
-        void createEdge(Node origin, Node destination);
+        Graph();
 
         //-------------- Getters --------------//
         vector<Node> getV(); //get vertices
+
+        //-------------- Inserting and connecting territories --------------//
+        void insertATerritory(Territory data);
+        void insertAndConnectTwoTerritories(Territory dataA, Territory dataB);
+        static void connectTwoNodes(Node A, Node B); //Invokes addEdge function for A and B nodes
+    private:
+        void x();
 };
