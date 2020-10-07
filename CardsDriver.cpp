03b2eb16b9958fb0feabcd3963e9af8f9af6bc10
Audray <67934088+AudrayM@users.noreply.github.com>
2020-10-06 22:17:55 -0400

    Final version :)

--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -1,5 +1,4 @@
 #include"Cards.cpp"
-#include"Cards.h"
 #include<iostream>
 #include<string>
 #include <vector>
@@ -9,20 +8,14 @@ using namespace std;
 
 int main() {
     Deck deck;
+
+    cout<< "Fill deck with 54 cards"<<endl;
     // Fill deck with 54 cards of random types
     srand(time(0));
     for (int i = 0; i < 54; i++) {
         int randomNb = rand() % 6;
-        switch (randomNb)
-        {
-        case 0: { deck.addCardToDeck(new Card(Card::SPY)); } break;
-        case 1: { deck.addCardToDeck(new Card(Card::BOMB)); } break;
-        case 2: { deck.addCardToDeck(new Card(Card::REINFORCEMENT)); } break;
-        case 3: { deck.addCardToDeck(new Card(Card::BLOCKADE)); } break;
-        case 4: { deck.addCardToDeck(new Card(Card::AIRLIFT)); } break;
-        case 5: { deck.addCardToDeck(new Card(Card::DIPLOMACY)); } break;
-        default: throw logic_error("invalid randomNb");
-        }
+        // Create new card, randomly assign type to card, & add card to deck
+        deck.addCardToDeck(new Card(randomNb));
     }
 
     Hand hand;
@@ -31,24 +24,22 @@ int main() {
     deck.print();
     cout<< "--------------------------"<<endl;
 
+    cout<< "Fill hand with 5 cards"<<endl;
     // Fill hand with 5 cards
     for (int i = 0; i < 5; i++) {
         deck.draw(&hand);
     }
-    
-    // Add another card to hand
-    Card card;
-    hand.addCardToHand(&card);
 
     hand.print();
     deck.print();
     cout<< "--------------------------"<<endl;
-    
-    // Play newly added card
-    card.play(&deck, &hand);
+
+    cout<< "Play first card in hand"<<endl;
+    // Play first card in hand
+    (hand.getHandCards()[0])->play(&deck, &hand);
     // TODO: Once merged use this instead
     // Player player;
-    // card.play(&deck, &hand, &player);
+    // (hand.getHandCards()[0]).play(&deck, &hand, &player);
 
     hand.print();
     deck.print();
