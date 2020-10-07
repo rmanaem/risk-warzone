875a5b6e7d4daed6fea10b705e2e52f614e94eaf
Audray <67934088+AudrayM@users.noreply.github.com>
2020-10-06 21:04:09 -0400

    Final version :)

--- Cards.cpp
+++ Cards.cpp
@@ -1,10 +1,3 @@
-
-#include<iostream>
-#include<string>
-#include <vector>
-#include<cstdlib>
-#include <algorithm> 
-using namespace std;
 #include"Cards.h"
 #include<iostream>
 #include<string>
@@ -13,143 +6,67 @@ using namespace std;
 #include <algorithm> 
 using namespace std;
 
-//--------------------------------Hand class----------------------------//
-//-------------- Constructors --------------//
-Hand::Hand(){
-}
 
-//-------------- Getters --------------//
-vector<Card*> Hand::getHandCards(){
-    return cardsInHand;
-}
-
-//-------------- Others --------------//
-void Hand::addCardToHand(Card* card){
-    cardsInHand.push_back(card);
-}
-void Hand::removeCardFromHand(int index){
-    cardsInHand.erase(cardsInHand.begin()+index);
-}
-
-void Hand::print() {
-    cout <<"This hand has " + to_string(countCards()) + " cards"<<endl;
-    for (int i = 0; i < countCards(); i++) {
-        cout <<"The card at position " + to_string(i) + " has type " + to_string(getHandCards()[i]->getCardType())<<endl;
-    }
-}
-
-int Hand::countCards(){
-    return cardsInHand.size();
-}
-
-int Hand::getCardIndex(Card* card) {
-    auto it = find(getHandCards().begin(), 
-                   getHandCards().end(), card); 
-  
-    // If element was found 
-    if (it != getHandCards().end()) { 
-        // calculating the index 
-        // of K 
-        int index = distance(getHandCards().begin(), 
-                             it); 
-        return index; 
-    } 
-    else { 
-        return -1; 
-    } 
-}
-
-//--------------------------------Deck class----------------------------//
-//-------------- Constructors --------------//
-Deck::Deck(){
-}
-
-//-------------- Getters --------------//
-vector<Card*> Deck::getDeckCards(){
-    return cardsInDeck;
-}
-
-//-------------- Others --------------//
-void Deck::addCardToDeck(Card* card){
-    cardsInDeck.push_back(card);
-}
-
-void Deck::removeCardFromDeck(int index){
-    cardsInDeck.erase(cardsInDeck.begin()+index);
-}
-
-void Deck::addCardToHand(Card* card, Hand* hand) {
-    hand->addCardToHand(card);
-}
-
-Card* Deck::getPtrRandomCardFromDeck() {
-    srand(time(0));
-    int RandIndex = rand() % cardsInDeck.size();
-    return cardsInDeck[RandIndex];
-}
-
-void Deck::draw(Hand* hand) {
-    // Generate random card index number
-    srand(time(0));
-    int RandIndex = rand() % cardsInDeck.size();
-    // Add selected card to hand
-    addCardToHand(cardsInDeck[RandIndex], hand);
-    // Remove selected card from deck
-    removeCardFromDeck(RandIndex);
-}
-
-int Deck::countCards(){
-    return cardsInDeck.size();
-}
-
-void Deck::print() {
-    cout <<"This deck has " + to_string(countCards()) + " cards"<<endl;
-    for (int i = 0; i < countCards(); i++) {
-        cout <<"The card at position " + to_string(i) + " has type " + to_string(getDeckCards()[i]->getCardType())<<endl;
-    }
-}
-
-//--------------------------------Card class----------------------------//
+//============================ Card Class ============================//
 //-------------- Constructors --------------//
 Card::Card(){
     this->cardType = SPY;
-}
-
-Card::Card(Deck* deck){
-    this->deck = deck;
-}
-
-Card::Card(Hand* hand){
-    this->hand = hand;
 }
 
 Card::Card(CardType cardType){
     this->cardType = cardType;
 }
 
-int Card::getCardType(){
+Card::Card(const Card& card){
+    cout << "Inside copy constructor of Card" << endl;
+    this->cardType = card.cardType;
+}
+
+Card& Card::operator=(const Card& card){
+    cout << "Inside operator = of Card" << endl;
+    this->cardType = card.cardType;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Card& card){
+    return stream << "Card details: type " << card.cardType << endl;
+}
+
+// Set card type with position of cardType in enum
+Card::Card(int intCardType){
+    switch (intCardType) {
+        case 0: { this->cardType = SPY; } break;
+        case 1: { this->cardType = BOMB; } break;
+        case 2: { this->cardType = REINFORCEMENT; } break;
+        case 3: { this->cardType = BLOCKADE; } break;
+        case 4: { this->cardType = AIRLIFT; } break;
+        case 5: { this->cardType = DIPLOMACY; } break;
+        default: throw logic_error("invalid nb");
+    }    
+}
+
+//-------------- Getters --------------//
+Card::CardType Card::getCardType() {
     return cardType;
 }
 
-Deck* Card::getDeck(){
-    return deck;
-}
-
-Hand* Card::getHand(){
-    return hand;
+string Card::getCardTypeString() {
+    string cardTypeString;
+    switch (this->cardType) {
+        case 0: { cardTypeString = "SPY"; } break;
+        case 1: { cardTypeString = "BOMB"; } break;
+        case 2: { cardTypeString = "REINFORCEMENT"; } break;
+        case 3: { cardTypeString = "BLOCKADE"; } break;
+        case 4: { cardTypeString = "AIRLIFT"; } break;
+        case 5: { cardTypeString = "DIPLOMACY"; } break;
+        default: throw logic_error("invalid nb");
+    }  
+    return cardTypeString;
 }
 
 //-------------- Setters --------------//
-void Card::setCardType(CardType newCardType){
-    this->cardType = newCardType;
-}
-
-void Card::setDeck(Deck* newDeck){
-    this->deck = newDeck;
-}
-
-void Card::setHand(Hand* newHand){
-    this->hand = newHand;
+void Card::setCardType(CardType cardType){
+    this->cardType = cardType;
 }
 
 //-------------- Others --------------//
@@ -170,3 +87,136 @@ void Card::play(Deck* deck, Hand* hand) {
 //     deck->addCardToDeck(this);
 //     hand->removeCardFromHand(hand->getCardIndex(this));
 // }
+
+
+//============================ Deck Class ============================//
+//-------------- Constructors --------------//
+Deck::Deck(){
+}
+
+Deck::Deck(vector<Card*> cardsInDeck){
+    this->cardsInDeck = cardsInDeck;
+}
+
+Deck::Deck(const Deck& deck){
+    cout << "Inside copy constructor of Deck" << endl;
+    this->cardsInDeck = deck.cardsInDeck;
+}
+
+Deck& Deck::operator=(const Deck& deck){
+    cout << "Inside operator = of Deck" << endl;
+    this->cardsInDeck = deck.cardsInDeck;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Deck& deck){
+    return stream << "Deck details: there are cards in the deck "<< endl;
+}
+
+//-------------- Getters --------------//
+vector<Card*> Deck::getDeckCards(){
+    return cardsInDeck;
+}
+
+//-------------- Setters --------------//
+void Deck::setDeckCards(vector<Card*> cardsInDeck){
+    this->cardsInDeck = cardsInDeck;
+}
+
+//-------------- Others --------------//
+void Deck::addCardToDeck(Card* card){
+    cardsInDeck.push_back(card);
+}
+
+void Deck::removeCardFromDeck(int index){
+    cardsInDeck.erase(cardsInDeck.begin()+index);
+}
+
+void Deck::draw(Hand* hand) {
+    // Generate random card index number
+    srand(time(0));
+    int RandIndex = rand() % nbCards();
+    // Add selected card to hand
+    hand->addCardToHand(cardsInDeck[RandIndex]);
+    // Remove selected card from deck
+    removeCardFromDeck(RandIndex);
+}
+
+int Deck::nbCards(){
+    return cardsInDeck.size();
+}
+
+void Deck::print() {
+    cout <<"This deck has " + to_string(nbCards()) + " cards"<<endl;
+    for (int i = 0; i < nbCards(); i++) {
+        cout <<"The card at position " + to_string(i) + " has type " + cardsInDeck[i]->getCardTypeString()<<endl;
+    }
+}
+
+
+//============================ Hand Class ============================//
+//-------------- Constructors --------------//
+Hand::Hand(){
+}
+
+Hand::Hand(vector<Card*> cardsInHand){
+    this->cardsInHand = cardsInHand;    
+}
+
+Hand::Hand(const Hand& hand){
+    cout << "Inside copy constructor of Hand" << endl;
+    this->cardsInHand = hand.cardsInHand;
+}
+
+Hand& Hand::operator=(const Hand& hand){
+    cout << "Inside operator = of Hand" << endl;
+    this->cardsInHand = hand.cardsInHand;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Hand& hand){
+    return stream << "Hand details: there are cards in the hand"<< endl;
+}
+
+//-------------- Getters --------------//
+vector<Card*> Hand::getHandCards(){
+    return cardsInHand;
+}
+
+//-------------- Setters --------------//
+void Hand::setHandCards(vector<Card*> cardsInHand){
+    this->cardsInHand = cardsInHand;
+}
+
+//-------------- Others --------------//
+void Hand::addCardToHand(Card* card){
+    cardsInHand.push_back(card);
+}
+void Hand::removeCardFromHand(int index){
+    cardsInHand.erase(cardsInHand.begin()+index);
+}
+
+void Hand::print() {
+    cout <<"This hand has " + to_string(nbCards()) + " cards"<<endl;
+    for (int i = 0; i < nbCards(); i++) {
+        cout <<"The card at position " + to_string(i) + " has type " + getHandCards()[i]->getCardTypeString()<<endl;
+    }
+}
+
+int Hand::nbCards(){
+    return cardsInHand.size();
+}
+
+int Hand::getCardIndex(Card* card) {
+    auto it = find(getHandCards().begin(), getHandCards().end(), card); 
+  
+    // If element was found 
+    if (it != getHandCards().end()) { 
+        // calculating the index 
+        int index = distance(getHandCards().begin(), it); 
+        return index; 
+    } 
+    else { 
+        return -1; 
+    } 
+}
