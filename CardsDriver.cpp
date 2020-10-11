f02985d15693c223a6c4effcf02726ca07647ec7
Audray <67934088+AudrayM@users.noreply.github.com>
2020-10-11 15:55:10 -0400

    Final version: Runs on Arman's computer
    Thanks Arman for all of the help!

--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -1,50 +1,84 @@
-#include"Cards.cpp"
-#include<iostream>
-#include<string>
+
+#include "Cards.h"
+#include "Player.h"
+#include "Orders.h"
+#include "Map.h"
+#include <iostream>
+#include <string>
 #include <vector>
-#include<cstdlib>
-#include <algorithm> 
+#include <cstdlib>
+#include <algorithm>
 using namespace std;
 
-int main() {
+int main()
+{
     {
-        Deck deck;
+        Map *validMap = new Map;
+        // Creating the South America and Africa continents
+        Continent *southAmerica = validMap->createContinent("South America");
 
-        cout<< "Fill deck with 54 cards"<<endl;
-        // Fill deck with 54 cards of random types
-        srand(time(0));
-        for (int i = 0; i < 54; i++) {
-            int randomNb = rand() % 6;
-            // Create new card, randomly assign type to card, & add card to deck
-            deck.addCardToDeck(new Card(randomNb));
-        }
+        // Creating the Territory pointers
+        Territory *ven = new Territory("Venzuela", southAmerica);
+        Territory *braz = new Territory("Brazil", southAmerica);
+        Territory *argen = new Territory("Argentina", southAmerica);
+        Territory *peru = new Territory("Peru", southAmerica);
 
-        Hand hand;
+        // Creating a vector of Territory pointers for the player
+        vector<Territory *> territoryv1;
+        territoryv1.push_back(ven);
+        territoryv1.push_back(braz);
+        territoryv1.push_back(argen);
+        territoryv1.push_back(peru);
 
-        hand.print();
-        deck.print();
-        cout<< "--------------------------"<<endl;
+        // Creating order pointers and a vector of order pointers for the player
+        Deploy *d1 = new Deploy;
+        Advance *a1 = new Advance;
+        Bomb *b1 = new Bomb;
+        vector<Order *> vo;
+        vo.push_back(d1);
+        vo.push_back(a1);
+        vo.push_back(b1);
+        OrdersList *olp = new OrdersList(vo);
 
-        cout<< "Fill hand with 5 cards"<<endl;
-        // Fill hand with 5 cards
-        for (int i = 0; i < 5; i++) {
-            deck.draw(&hand);
-        }
+        // Creating card pointers and a vector of card pointers for the player
+        Card *card1 = new Card(2);
+        Card *card2 = new Card(1);
+        Card *card3 = new Card(3);
+        vector<Card *> vcards1;
+        vcards1.push_back(card1);
+        vcards1.push_back(card2);
+        vcards1.push_back(card3);
+        Hand *handp = new Hand(vcards1);
 
-        hand.print();
-        deck.print();
-        cout<< "--------------------------"<<endl;
+        // Creating card pointers and a vector of card pointers for the deck
+        Card *card4 = new Card(0);
+        Card *card5 = new Card(1);
+        Card *card6 = new Card(3);
+        vector<Card *> vdeck1;
+        vdeck1.push_back(card4);
+        vdeck1.push_back(card5);
+        vdeck1.push_back(card6);
+        Deck *deckp = new Deck(vdeck1);
 
-        cout<< "Play first card in hand"<<endl;
-        // Play first card in hand
-        (hand.getHandCards()[0])->play(&deck, &hand);
-        // TODO: Once merged use this instead
-        // Player player;
-        // (hand.getHandCards()[0]).play(&deck, &hand, &player);
-
-        hand.print();
-        deck.print();
-
-        return(0);
+        // Initializing a Player using the initalized data memebers
+        Player *p1 = new Player(4, territoryv1, handp, olp);
+        
+        // Displaying deck and hand
+        cout << "Before anything happens" << endl;
+        handp->print();
+        deckp->print();
+        
+        // Playing first card and displaying deck and hand
+        (*card1).play(deckp, p1);
+        cout << "After playing card 1 (at position 0)" << endl;
+        handp->print();
+        deckp->print();
+        
+        // Drawing first card and displaying deck and hand
+        deckp->draw(handp);
+        cout << "After drawing 1 card from deck to player's hand" << endl;
+        handp->print();
+        deckp->print();
+        return (0);
     }
-} 
+}
