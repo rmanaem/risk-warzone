815e475d066edec5bf85ddb4a77ebe05209a4525
Arman <armanjahanpour7@gmail.com>
2020-11-18 11:33:22 -0500

    Adding unattackable data memebr

--- Player/Player.h
+++ Player/Player.h
@@ -20,17 +20,19 @@ private:
     //-------------- Data members --------------//
     int playerId;
     int reinforcementPool;
-    //vector of Territories owned by Player
+    // Vector of players that the player can't attack for a turn, related to negotiate order logic
+    std::vector<Player*> unattackablePlayers;
+    // vector of Territories owned by Player
     std::vector<Territory *> territoriesOwned;
-    //ptr of type Hand that contains a vector of ptr of Cards that belong to Player
+    // ptr of type Hand that contains a vector of ptr of Cards that belong to Player
     Hand *cards;
-    //ptr of type OrdersList that contains a vector of ptr of Orders that belong to Player
+    // ptr of type OrdersList that contains a vector of ptr of Orders that belong to Player
     OrdersList *orders;
 
 public:
     //-------------- Constructors --------------//
     Player();
-    Player(int playerId, int reinforcementPool, std::vector<Territory *> territoriesOwned, Hand *cards, OrdersList *orders);
+    Player(int playerId, int reinforcementPool, std::vector<Player*> unattackablePlayers ,std::vector<Territory *> territoriesOwned, Hand *cards, OrdersList *orders);
     Player(const Player &e);
 
     //-------------- Destructor --------------//
@@ -42,6 +44,7 @@ public:
     //-------------- Accessor methods --------------//
     int getPlayerId();
     int getReinforcementPool();
+    std::vector<Player *> getUnattackablePlayers();
     std::vector<Territory *> getTerritoriesOwned();
     Hand *getCards();
     OrdersList *getOrders();
@@ -49,6 +52,7 @@ public:
     //-------------- Mutator methods --------------//
     void setPlayerId(int playerId);
     void setReinforcementPool(int reinforcementPool);
+    void setUnattackablePlayers(std::vector<Player*> unattackblePlayers);
     void setTerritoriesOwned(std::vector<Territory *> territoriesOwned);
     void setCards(Hand *cards);
     void setOrders(OrdersList *orders);
