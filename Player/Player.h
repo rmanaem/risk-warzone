b5660c071b1f325859dba83c588ae6f59913658d
Audray <67934088+AudrayM@users.noreply.github.com>
2020-11-28 08:10:19 -0500

    Adding PlayerStrategy

--- Player/Player.h
+++ Player/Player.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#include "Map.h"
-#include "Orders.h"
-#include "Cards.h"
+#include "../Map/Map.h"
+#include "../Orders.h"
+#include "../Cards.h"
+#include "../GameObservers.h"
+#include "../GameEngine/GameEngine.h"
+#include "../PlayerStrategy.h"
 #include <iostream>
 #include <vector>
 
 //============================ Forward declarations  ============================//
 class Card;
 class Hand;
+class GameStarter;
 
 //============================ Player Class ============================//
 class Player
@@ -17,18 +21,21 @@ class Player
 private:
     //-------------- Data members --------------//
     int playerId;
-    int nbArmies;
-    //vector of Territories owned by Player
+    int reinforcementPool;
+    // Vector of players that the player can't attack for a turn, related to negotiate order logic
+    std::vector<Player*> unattackablePlayers;
+    // vector of Territories owned by Player
     std::vector<Territory *> territoriesOwned;
-    //ptr of type Hand that contains a vector of ptr of Cards that belong to Player
+    // ptr of type Hand that contains a vector of ptr of Cards that belong to Player
     Hand *cards;
-    //ptr of type OrdersList that contains a vector of ptr of Orders that belong to Player
+    // ptr of type OrdersList that contains a vector of ptr of Orders that belong to Player
     OrdersList *orders;
-
+    Phase *phase;
+    PlayerStrategy* strategy;
 public:
     //-------------- Constructors --------------//
     Player();
-    Player(int playerId, int nbArmies, std::vector<Territory *> territoriesOwned, Hand *cards, OrdersList *orders);
+    Player(int playerId, int reinforcementPool, std::vector<Player*> unattackablePlayers ,std::vector<Territory *> territoriesOwned, Hand *cards, OrdersList *orders, PlayerStrategy *strategy = nullptr);
     Player(const Player &e);
 
     //-------------- Destructor --------------//
@@ -39,17 +46,22 @@ public:
 
     //-------------- Accessor methods --------------//
     int getPlayerId();
-    int getNbArmies();
+    Phase getPhase();
+    int getReinforcementPool();
+    std::vector<Player *> getUnattackablePlayers();
     std::vector<Territory *> getTerritoriesOwned();
     Hand *getCards();
     OrdersList *getOrders();
+    PlayerStrategy *getStrategy();
 
     //-------------- Mutator methods --------------//
     void setPlayerId(int playerId);
-    void setNbArmies(int nbArmies);
+    void setReinforcementPool(int reinforcementPool);
+    void setUnattackablePlayers(std::vector<Player*> unattackblePlayers);
     void setTerritoriesOwned(std::vector<Territory *> territoriesOwned);
     void setCards(Hand *cards);
     void setOrders(OrdersList *orders);
+    void setStrategy(PlayerStrategy* strategy);
 
     //-------------- toDefend method --------------//
     std::vector<Territory *> toDefend();
@@ -58,7 +70,7 @@ public:
     std::vector<Territory *> toAttack(Map *map);
 
     //-------------- issueOrder method --------------//
-    void issueOrder();
+    void issueOrder(Map *map, GameStarter *gameStarter);
 
     //-------------- Stream insertion Operator --------------//
     friend std::ostream &operator<<(std::ostream &out, const Player &e);
