a802e5003e2fc7e0205fcb13fac4b97f39820379
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-12-02 18:32:01 -0500

    Remove commented code and write some more comments

--- MapLoader/FileReaderAdapterDriver.cpp
+++ MapLoader/FileReaderAdapterDriver.cpp
@@ -15,34 +15,24 @@
 using namespace std;
 
 int main() {
-//    ConquestFileReader* conquestLoader;
-//    MapLoader* dominationLoader;
-//
-//    //Domination Map Loader
-//    Map* dominationMap = new Map(dominationLoader->parseMap("astadt.map"));
-//
-//    cout<<"\n\n--------- Using Adapter for conquest map ---------\n"<<endl;
-//
-//    //create an adapter that adapts a MapLoader to ConquestFileReader
-//    ConquestFileReaderAdapter* adapter = new ConquestFileReaderAdapter(*conquestLoader);
-//
-//    Map* conquestMap = new Map(adapter->parseMap("Unconnected.map"));
     ConquestFileReader* conquestLoader;
+    //ask user about the map type
     std::map<int, string> struc=conquestLoader->selectMap();
     int mapType=struc.begin()->first; //load the map
     string selectedMap = struc.begin()->second;
     MapLoader* loader;
-    Map* myGraph;
+    Map* myGraph;//loaded graph
+    
+    //check if conquest or domination
     if(mapType==2){
-
+        //use the adapter
         ConquestFileReaderAdapter* adapter = new ConquestFileReaderAdapter(*conquestLoader);
         myGraph = new Map(adapter->parseMap(selectedMap));
     }
-    else
+    else//original (i.e. domination) MapLoader
         myGraph = new Map(loader->parseMap(selectedMap));
 
         delete myGraph;
-
 
     return 0;
 }
